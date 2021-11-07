fe2b8d26d245c3a4c648e02a3f22f903db2471f6
Changbai Guo <33477119+CBVeLuMe@users.noreply.github.com>
2021-11-07 01:38:44 -0400

    Restyle the code, fix many bugs

--- BPlusTree.cpp
+++ BPlusTree.cpp
@@ -1,185 +1,185 @@
 // BPlusTree.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <fstream>
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <vector>
 
-using namespace std;
+const int gOrder = 4;//the max children of a internal node, leaf also has 4, but last 1 for sibling
 
-//Base class for internal and leaf
+//Base class for internal and leaf node
 class node {
 public:
-
-	virtual void set_keys(string* keys) = 0;
-	virtual string* get_keys() = 0;
-	
+	node();
+	node(node* parentPtr);
+	std::string get_first_key();
+	int get_keys_size();
+	virtual bool is_full() = 0;
+	//Parse key to compare
+	static bool is_same_key(std::string sub, std::string obj);
+	static bool is_larger_key(std::string sub, std::string obj);
+private:
 protected:
-	string mKeys[3];//round_up(order / 2) - 1 <= number of keys <= order - 1
+	node* mParentPtr;
+	std::vector<std::string> mKeys;
+	virtual void push_key_back(std::string newKey);
 };
+
+node::node() {
+	mParentPtr = NULL;
+	mKeys.reserve(gOrder - 1);//round_up(order / 2) - 1 <= number of keys <= order - 1
+}
+
+node::node(node* parentPtr) {
+	mParentPtr = parentPtr;
+	mKeys.reserve(gOrder - 1);//round_up(order / 2) - 1 <= number of keys <= order - 1
+}
+
+std::string node::get_first_key() {//should be min key
+	return mKeys.front();
+}
+
+int node::get_keys_size() {
+	return mKeys.size();
+}
+
+bool node::is_same_key(std::string sub, std::string obj) {
+	if (sub == obj) {
+		return true;
+		std::cout << "Warning: Key cannot be same." << std::endl;
+	}
+	return false;
+}
+
+bool node::is_larger_key(std::string sub, std::string obj) {
+	if (is_same_key(sub, obj)) return false;
+	for (int i = 0; i < sub.length(); i++) {
+		if (sub[i] > obj[i]) return true;
+	}
+	return false;
+}
+
+void node::push_key_back(std::string newKey) {
+	mKeys.push_back(newKey);
+}
 
 class root : public node {
-
+	using node::node;
+	bool is_full() {
+		return true;
+	}
 };
 
-class internal : public node {
+class non_leaf : public node {
+	using node::node;
+	bool is_full() {
+		return true;
+	}
+};
+
+class leaf : public node {
+	using node::node;
 public:
-	void set_keys(string* newKeys);
-	string* get_keys();
-	void set_ptrs(node* newPtrs);
-	internal(string* newKeys, node* newPtrs) {
-		set_keys(newKeys);
-		set_ptrs(newPtrs);
-	}
+	leaf();
+	void set_pair(std::string newKey, std::string newRecord);
+	void set_next_sibling_ptr(leaf* nextPtr);
+	leaf* get_next_sibling_ptr();
+	bool is_full();
+	void print();
 private:
-	node* mPtrs[4];//round_up(order / 2) <= number of children <= order
+	void push_record_back(std::string newRecord);
+	std::vector<std::string> mRecords;
+	leaf* mNextSiblingPtr = NULL;
 };
 
-void internal::set_keys(string* newKeys) {
-	for (int i = 0; i < sizeof(mKeys); i++) {
-		mKeys[i] = newKeys[i];
-	}
+leaf::leaf() : node() {
+	mRecords.reserve(gOrder - 1);
 }
 
-string* internal::get_keys() {
-	return mKeys;
-}
-
-void internal::set_ptrs(node* newPtrs) {
-	for (int i = 0; i < sizeof(mPtrs); i++) {
-		mPtrs[i] = &newPtrs[i];
-	}
-}
-
-class leaf : public node {//Or Leaf Node
-public:
-	void set_keys(string* newKeys);
-	void set_keys(string newKey, int index);
-	string* get_keys();
-	void set_ptrs(string* newPtrs);
-	string* get_ptrs(void);
-	void set_next_sibling(leaf* nextPtr);
-	leaf* get_next_sibling(void);
-	void print(void);
-	void print(int i);
-private:
-	string mPtrs[3];//Todo: should be fstream pointer
-	leaf* mNextSibling = NULL;
-};
-
-void leaf::set_keys(string* newKeys) {
-	for (int i = 0; i < sizeof(mKeys); i++) {
-		mKeys[i] = newKeys[i];
-	}
-}
-
-void leaf::set_keys(string newKey, int index) {
-	mKeys[index] = newKey;
-}
-
-string* leaf::get_keys() {
-	return mKeys;
-}
-
-void leaf::set_ptrs(string* newPtrs) {
-	for (int i = 0; i < sizeof(mPtrs); i++) {
-		mPtrs[i] = newPtrs[i];
-	}
-}
-
-string* leaf::get_ptrs() {
-	return mPtrs;
-}
-
-void leaf::set_next_sibling(leaf* nextPtr) {
-	mNextSibling = nextPtr;
-}
-
-leaf* leaf::get_next_sibling() {
-	return mNextSibling;
-}
-
-void leaf::print() {
-	std::cout << " key: " + mKeys[0] << "\n";
-	std::cout << " key: " + mKeys[1] << "\n";
-	std::cout << " key: " + mKeys[2] << "\n";
+void leaf::set_pair(std::string newkey, std::string newRecord) {
+	push_key_back(newkey);
+	push_record_back(newRecord);
 	//for (int i = 0; i < sizeof(mKeys); i++) {
-	//	std::cout << " key: " + mKeys[1] + " record: " << "\n";
+		////should compare here
 	//}
 }
 
-void leaf::print(int i) {
-	std::cout << " key: " + mKeys[i] + " record: " + mPtrs[i] << "\n";
+void leaf::set_next_sibling_ptr(leaf* nextPtr) {
+	mNextSiblingPtr = nextPtr;
+}
+
+leaf* leaf::get_next_sibling_ptr() {
+	return mNextSiblingPtr;
+}
+
+void leaf::push_record_back(std::string newRecord) {
+	mRecords.push_back(newRecord);
+}
+
+bool leaf::is_full() {
+	return mRecords.size() == gOrder - 1;
+}
+
+void leaf::print() {
+	std::vector<std::string>::iterator keysIt = mKeys.begin();
+	std::vector<std::string>::iterator recordsIt = mRecords.begin();
+	while (recordsIt != mRecords.end()) {
+		std::cout << *keysIt << "    " << *recordsIt << " | ";
+		keysIt++;
+		recordsIt++;
+	}
+	std::cout << std::endl;
 }
 
 class b_plus_tree {
 public:
-	int get_order();
-	leaf* Initial(string);
-	string trim(string);
+	leaf* initial(std::string);
+	std::string trim(std::string);
 	void traverse(leaf* header);
-	//char* parse_key(string);
 private:
-	int order = 4;
+	leaf* mHeader;
 };
 
-int b_plus_tree::get_order() {
-	return order;
-}
-
-leaf* b_plus_tree::Initial(string filePath) {
-	fstream fsfile;
-	string line, key, record;
-	fsfile.open(filePath);
-	int counter = 0;
-	leaf* header = new leaf();
-	leaf* leafPtr = header;
-	while (getline(fsfile, line)) {
-		istringstream iss(line);
+//should ensure being sequential (next larger than cur, not same)
+leaf* b_plus_tree::initial(std::string filePath) {
+	std::ifstream infile(filePath);
+	std::string line, key, record;//record should be ptr to fstream address
+	leaf* headerPtr = new leaf();
+	leaf* leafPtr = headerPtr;
+	for (std::string line; std::getline(infile, line); ) {
 		key = trim(line.substr(0, 7));
-		//record = trim(line.substr(15, 80));
-		//char keys [8] = strncpy_s(keys, key.c_str(), sizeof(keys));
-
-		if (counter != 0 && counter % (get_order() - 1) == 0) {
-			leafPtr->print();
-			leaf* nextPtr = new leaf();
-			leafPtr->set_next_sibling(nextPtr);
-			leafPtr = nextPtr;
+		record = trim(line.substr(15, 80));
+		if (!(leafPtr->is_full())) {
+			leafPtr->set_pair(key, record);
 		}
-		leafPtr->set_keys(key, (counter % (get_order() - 1)));
-		counter++;
+		else {
+			leaf* nextPtr = new leaf();
+			leafPtr->set_next_sibling_ptr(nextPtr);
+			leafPtr = nextPtr;
+			leafPtr->set_pair(key, record);
+		}
 	}
-	return header;
+	return headerPtr;
 }
 
-string b_plus_tree::trim(string str) {
+std::string b_plus_tree::trim(std::string str) {
 	str.erase(0, str.find_first_not_of(" "));
 	str.erase(str.find_last_not_of(" ") + 1);
 	return str;
 }
 
-void b_plus_tree::traverse(leaf* header) {
-	while (header->get_next_sibling() == NULL) {
-		header->print();
-		leaf* nextPtr = header->get_next_sibling();
+void b_plus_tree::traverse(leaf* headerPtr) {
+	headerPtr->print();
+	while (headerPtr->get_next_sibling_ptr() != NULL) {
+		headerPtr = headerPtr->get_next_sibling_ptr();
+		headerPtr->print();
 	}
 }
 
 int main()
 {
 	b_plus_tree tree;
-	leaf* header = tree.Initial("D:\\CBVeLuMe\\Desktop\\WorkNote\\algorithm-concepts\\BPlusTree\\partfile.txt");
-	tree.traverse(header);
+	leaf* headerPtr = tree.initial("D:\\CBVeLuMe\\Desktop\\WorkNote\\algorithm-concepts\\BPlusTree\\temp.txt");
+	tree.traverse(headerPtr);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
